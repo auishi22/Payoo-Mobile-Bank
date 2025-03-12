7ecb6d6f2bfbf242ecc84ae5b0e794f0781b2779
auishi22 <auishee222@gmail.com>
2025-03-12 14:53:48 +0600

    added homepage

--- js/login.js
+++ js/login.js
@@ -6,4 +6,11 @@ document.getElementById("btn-login").addEventListener("click", function (e) {
 
   const digitNumber = document.getElementById("digit-number").value;
   console.log(digitNumber);
+
+  if (phoneNumber === "5" && digitNumber === "1234") {
+    console.log("You are logged in");
+    window.location.href='/home.html'
+  } else {
+    alert("enter wrong phone number or pin number. please try again");
+  }
 });
